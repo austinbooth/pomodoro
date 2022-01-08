1b53305908dc0c7b6d3212da7a5c58f59371e10a
Austin Booth <austinbooth78@gmail.com>
2022-01-08 20:24:08 +0000

    adds code for a work period followed by a rest period

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,28 @@ import ControlButtons from './components/ControlButtons'
 import { State } from './types'
 
 const App: FC = () => {
-  const [workTime, setWorkTime] = useState(60)
-  const [restTime, setRestTime] = useState(10)
+  const workSeconds = 60
+  const restSeconds = 10
+  const [workTime, setWorkTime] = useState(workSeconds)
+  const [restTime, setRestTime] = useState(restSeconds)
   const [state, setState] = useState<State>('not-started')
   const [intervalRef, setIntervalRef] = useState<NodeJS.Timer>()
-  const start = () => setState('work')
-  const pause = () => setState('work-paused')
-  const decrementTime = () => setWorkTime(time => time - 1)
+  const start = () => {
+    if (state === 'not-started' || state === 'work-paused') {
+      setState('work')
+    } else {
+      setState('rest')
+    }
+  }
+  const pause = () => {
+    if (state === 'work') {
+      setState('work-paused')
+    } else {
+      setState('rest-paused')
+    }
+  }
+  const decrementWorkTime = () => setWorkTime(time => time - 1)
+  const decrementRestTime = () => setRestTime(time => time - 1)
   const stopCountdown = () => {
     if (intervalRef) {
       clearInterval(intervalRef)
@@ -20,7 +35,7 @@ const App: FC = () => {
 
   useEffect(() => {
     if (state === 'work') {
-      const interval_ref = setInterval(decrementTime, 1000)
+      const interval_ref = setInterval(decrementWorkTime, 1000)
       setIntervalRef(interval_ref)
 
       return () => clearInterval(interval_ref)
@@ -28,18 +43,41 @@ const App: FC = () => {
     if (state === 'work-paused' && intervalRef) {
       clearInterval(intervalRef)
     }
+    if (state === 'rest') {
+      const interval_ref = setInterval(decrementRestTime, 1000)
+      setIntervalRef(interval_ref)
+
+      return () => clearInterval(interval_ref)
+    }
+    if (state === 'rest-paused' && intervalRef) {
+      clearInterval(intervalRef)
+    }
   }, [state])
 
   useEffect(() => {
-    if (workTime === 0) {
-      setState('not-started')
-      setWorkTime(60)
+    if (workTime === 0 && state === 'work') {
+      setState('rest')
+      setWorkTime(workSeconds)
     }
-  }, [workTime])
+    if (restTime === 0 && state === 'rest') {
+      setState('not-started')
+      setRestTime(restSeconds)
+    }
+  }, [workTime, restTime])
+
+  const getTime = (state: State, workTime: number, restTime: number) => {
+    if (state === 'work' || state === 'work-paused') {
+      return workTime * 1000
+    }
+    if (state === 'rest' || state === 'rest-paused') {
+      return restTime * 1000
+    }
+    return 0 // state must be 'not-started'
+  }
 
   return (
     <div>
-      <Countdown time={state === 'not-started' ? 0 : workTime * 1000} stopCountdown={stopCountdown} />
+      <Countdown time={getTime(state, workTime, restTime)} stopCountdown={stopCountdown} />
       <ControlButtons state={state} start={start} pause={pause} />
     </div>
   );
